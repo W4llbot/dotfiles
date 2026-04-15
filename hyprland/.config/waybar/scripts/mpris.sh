7f9f4b03ebb0cf38c999dd123c3e9a47f9e11201
W4llbot <cahchenanhong@gmail.com>
2026-04-15 12:41:53 +0800

    fixed mpris not showing up in waybar if no players are initiated

---
+++ hyprland/.config/waybar/scripts/mpris.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+player_status=$(playerctl status 2>/dev/null)
+
+if [ "$player_status" = "Playing" ]; then
+    echo "{\"text\": \"  ⏸ | $(playerctl metadata --format '{{artist}} - {{title}}')\", \"class\": \"playing\"}"
+elif [ "$player_status" = "Paused" ]; then
+    echo "{\"text\": \"  ▶ | $(playerctl metadata --format '{{artist}} - {{title}}')\", \"class\": \"playing\"}"
+else
+    echo "{\"text\": \"  ▶ | Not playing\", \"class\": \"stopped\"}"
+fi
+        # "format": "  {status_icon} | {dynamic}",
+        # "format-disconnected": "  ▶ | No players",
+        # "interval": 1,
+        # "dynamic-len": 40,
+        # "status-icons": {
+        #     "playing": "⏸",
+        #     "paused": "▶",
+        #     "stopped": ""
+        # },
+        # "dynamic-order": ["artist", "title"]
